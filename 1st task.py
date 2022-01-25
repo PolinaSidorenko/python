d29696c01dc6727028a1e8d1d29f0e49a76c743e
PolinaSidorenko <psidorenko1707@gmail.com>
2022-01-25 16:54:41 +0300

    1

---
+++ 1st task.py
@@ -0,0 +1,9 @@
+#Поработайте с переменными, создайте несколько, выведите на экран.
+#Запросите у пользователя некоторые числа и строки и сохраните в переменные, затем выведите на экран.
+text = "hey there!"
+number = 123123
+print(text[0])
+print(text, number)
+number = int(input('Input the number: '))
+text = str(input('Input the sentence: '))
+print('The result: ', number, text)
